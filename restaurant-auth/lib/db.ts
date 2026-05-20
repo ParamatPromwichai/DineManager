412f471d398280b711a8a16b22823ed39a8b6c62
Paramat <153594744+ParamatPromwichai@users.noreply.github.com>
2026-05-21 01:16:07 +0700

    change to tidb

--- restaurant-auth/lib/db.ts
+++ restaurant-auth/lib/db.ts
@@ -2,18 +2,14 @@ import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
   host: process.env.DB_HOST!,
-  port: Number(process.env.DB_PORT || 3306), // XAMPP ส่วนใหญ่ใช้ 3306
+  port: Number(process.env.DB_PORT || 4000),
   user: process.env.DB_USER!,
-  password: process.env.DB_PASSWORD!,
+  password: process.env.DB_PASSWORD!, // ดึงรหัสผ่านใหม่จาก .env
   database: process.env.DB_NAME!,
-
-  // ✅ แก้ไขตรงนี้: ถ้าไม่มี DB_SSL_CA ให้เป็น undefined (ไม่ใช้ SSL)
-  ssl: process.env.DB_SSL_CA ? {
-    ca: process.env.DB_SSL_CA,
+  ssl: {
     rejectUnauthorized: true,
     minVersion: 'TLSv1.2',
-  } : undefined,
-
+  },
   waitForConnections: true,
   connectionLimit: 5,
 });
