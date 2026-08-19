6006a5148f0039786970dd19b82276b538000d5d
paramat <paramat2017games@gmail.com>
2026-08-19 20:07:53 +0700

    fix vercel build output

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'standalone',
   turbopack: {},
   async headers() {
     return [
